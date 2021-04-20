b8fca6ad991ca12ebc0e19b6b2f795922607067d
Long Nguyen <long.nguyen@eastgate-software.com>
2021-04-20 15:19:21 +0700

    remove fix code cpu

--- ai/face/face_detection/detect.py
+++ ai/face/face_detection/detect.py
@@ -51,7 +51,7 @@ class Detect:
         return None
 
     def calc_embedding(self, face_image):
-        return self.embedding_model(face_image.unsqueeze(0)).detach().numpy()
+        return self.embedding_model(face_image.unsqueeze(0)).to(device).detach().numpy()
 
     def face_detect(self, path, save=False):
         # Load Folder as Dataset
